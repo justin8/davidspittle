f19073a107913610f0ac4fb659dfe71d71ea24a6
Justin Dray ✌️ 🙌 <justin@dray.be>
2019-08-19 15:25:59 +1000

    Change to managed policy for cloudwatch agent permissions

--- cdk/backups/lib/s3-backup-stack.ts
+++ cdk/backups/lib/s3-backup-stack.ts
@@ -2,9 +2,10 @@ import cdk = require('@aws-cdk/core');
 import iam = require('@aws-cdk/aws-iam');
 import {Alarm, ComparisonOperator, Metric} from '@aws-cdk/aws-cloudwatch';
 import {SnsAction} from '@aws-cdk/aws-cloudwatch-actions';
-import {PolicyStatement} from '@aws-cdk/aws-iam';
+import {PolicyStatement, ManagedPolicy} from '@aws-cdk/aws-iam';
 import {FilterPattern, LogGroup, MetricFilter} from '@aws-cdk/aws-logs';
 import {Topic} from '@aws-cdk/aws-sns';
+import {MANAGED_POLICIES} from 'cdk-constants';
 
 export interface s3BackupStackProps {
   alarmsTopicName: string;
@@ -26,34 +27,24 @@ export class s3BackupStack extends cdk.Stack {
     // IAM
     const readynas = new iam.User(this, 'readynas', {userName: 'readynas'});
 
-    const cloudwatchLogsWriter = new iam.Policy(this, 'cloudwatchLogsWriter', {
-      policyName: 'cloudwatchLogsWriter',
+    const policies = [new iam.Policy(this, 's3BackupsWriterPolicy', {
+      policyName: 's3BackupsWriterPolicy',
       statements: [new PolicyStatement({
-        actions: [
-          'logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents',
-          'logs:DescribeLogStreams'
-        ],
+        actions: ['s3:*'],
         effect: iam.Effect.ALLOW,
-        resources: ['arn:aws:logs:*']
+        resources: [
+          'arn:aws:s3:::davidspittlebackups',
+          'arn:aws:s3:::davidspittlebackups/*'
+        ]
       })]
-    });
+    })];
 
-    const s3BackupsWriterPolicy =
-        new iam.Policy(this, 's3BackupsWriterPolicy', {
-          policyName: 's3BackupsWriterPolicy',
-          statements: [new PolicyStatement({
-            actions: ['s3:*'],
-            effect: iam.Effect.ALLOW,
-            resources: [
-              'arn:aws:s3:::davidspittlebackups',
-              'arn:aws:s3:::davidspittlebackups/*'
-            ]
-          })]
-        });
-
-    for (let policy of [cloudwatchLogsWriter, s3BackupsWriterPolicy]) {
+    for (let policy of policies) {
       readynas.attachInlinePolicy(policy);
     }
+
+    readynas.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName(
+        MANAGED_POLICIES.CLOUD_WATCH_AGENT_SERVER_POLICY))
 
     // Logs
     const logGroup = new LogGroup(
